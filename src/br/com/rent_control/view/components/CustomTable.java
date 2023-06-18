f369d0b30be92b484c4f86662c83ee2949024a3c
pauloribeiro <ribeiro.paulotm@gmail.com>
2023-06-18 15:13:55 -0400

    feat: flex table

--- src/br/com/rent_control/view/components/CustomTable.java
+++ src/br/com/rent_control/view/components/CustomTable.java
@@ -2,84 +2,89 @@ package br.com.rent_control.view.components;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
 
-import br.com.rent_control.model.vo.User;
+import br.com.rent_control.view.customer.CustomerManagementScreen.ActionButton;
 
 
 public class CustomTable extends JPanel {
-	private ImageIcon imageIcon;
-	private User user;
-	
-	public CustomTable() {
+	private static final long serialVersionUID = 1L;
+	private ActionButton actionButton;
+	public CustomTable(ActionButton actionButton, Object[][] data) {
+		this.actionButton = actionButton;
 		// TODO Auto-generated constructor stub
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		setBackground(Color.white);
-		imageIcon =  new ImageIcon("src/br/com/rent_control/view/images/tableHeader.png");
-		JLabel label = new JLabel(imageIcon);
-		label.setBounds(30, 100, 789, 43);
-		add(label);
 		
-		user = new User("ayumi aoki", "brasil", "04761241233", 123, 32567677, "13032003", 69088413, 137,"jorge teixeira", "rua malvarisco", "manaus", "amazonas");
-		User user2 = new User("Paulo Victor", "Brasil", "70214855228", 785, 32367689, "13032003", 69088413, 137,"Jorge Teixeira", "Rua Malvarisco", "Manaus", "Amazonas");
-		List<User> users = new ArrayList<>();
-		users.add(user2);
-		users.add(user);
-		users.add(user2);
-		users.add(user);
-		users.add(user2);
-		users.add(user);
-		users.add(user2);
-		users.add(user);
-		users.add(user2);
-		users.add(user);
-		users.add(user2);
-		users.add(user);
-		users.add(user);
-		users.add(user2);
-		users.add(user);
-		users.add(user2);
-		users.add(user);
-		users.add(user2);
-		users.add(user);
-		int y = 154;
-		for (User user : users) {
-			displayLabel(user.getName(), 49, y);
-			displayLabel(user.getCpf(), 222, y);
-			displayLabel(Long.toString(user.getLicenseNumber()), 378, y);
-			displayLabel(user.getDateOfBirth(), 546, y);
-			displayIcon("edit.png", ColorUtils.WARNING_COLOR, 679, y);
-			displayIcon("trash.png", ColorUtils.DANGER_COLOR,750, y);
-			y += 38;
+		GridLayout layout = new GridLayout(1,  data[0].length + 2);
+		layout.setHgap(50);
+		
+		for (int row = 0; row < data.length; row++) {
+			JPanel panel = new JPanel(layout);
+			panel.setBorder(null);
+			panel.setBackground(Color.white);
+			panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+			if(row == 0) {
+				panel.setBackground(new Color(243, 246, 249));
+			}
+			for (int col = 0; col < data[row].length; col++) {
+			    Object cell = data[row][col];
+			    
+			    if (col + 1 == data[row].length && row == 0) {
+			        displayLabel("Editar", panel, Font.BOLD);
+			        displayLabel("Excluir", panel, Font.BOLD);
+			    } else {
+			        if (col == data[row].length - 1) {
+			            displayIcon(cell.toString(), "edit",  panel);
+			            displayIcon(cell.toString(), "trash", panel);
+			        } else {
+			        	if(row == 0) {
+			        		displayLabel(cell.toString(), panel, Font.BOLD);
+			        	}else {
+			        		displayLabel(cell.toString(), panel, 400);
+			        	}
+			            
+			        }
+			    }
+			}
+
+	        add(panel);
+	    }
+		
+		if(data.length < 15) {
+			add(Box.createVerticalStrut(31 * (15 - data.length)));
 		}
-		
-		
-		
-		
 	}
 	
-	private void displayLabel(String label, int x, int y) {
+	private void displayLabel(String label, JPanel panel, int fontWeight) {
 		JLabel newLabel = new JLabel(label);
-		newLabel.setBounds(x, y, 165, 24);
-		newLabel.setFont(newLabel.getFont().deriveFont(Font.BOLD, 14));
-		add(newLabel);
+		newLabel.setFont(newLabel.getFont().deriveFont(fontWeight, 14));
+		panel.add(newLabel);
 	}
 	
-	private void displayIcon(String path, Color color, int x, int y) {
+	private void displayIcon(String id, String path, JPanel panel) {
 		JButton btn = new JButton();
-		btn.setIcon(new ImageIcon("src/br/com/rent_control/view/images/" + path));
+		btn.setIcon(new ImageIcon("src/br/com/rent_control/view/images/" + path + ".png"));
 		btn.setForeground(Color.WHITE);
-		btn.setBounds(x, y - 6, 40, 34);
-		btn.setBackground(color);
+		btn.setBackground(path.equals("edit") ? ColorUtils.WARNING_COLOR : ColorUtils.DANGER_COLOR);
 		btn.setBorder(null);
-		add(btn);
+		panel.add(btn);
+		
+		btn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				actionButton.action(id, path);
+			}
+		});
 	}
 }
